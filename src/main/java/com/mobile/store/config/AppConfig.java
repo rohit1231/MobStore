14e5839ac825027d6ce3a1f969b1d1035d1107e7
rohit1231 <nrohitnaik@gmail.com>
2016-03-19 03:47:18 -0700

    initial checkins

---
+++ src/main/java/com/mobile/store/config/AppConfig.java
@@ -0,0 +1,53 @@
+package com.mobile.store.config;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.mobile.store.*")
+public class AppConfig extends WebMvcConfigurerAdapter{
+	
+	@Bean
+	public ViewResolver viewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setViewClass(JstlView.class);
+		viewResolver.setPrefix("/WEB-INF/views/");
+		viewResolver.setSuffix(".jsp");
+
+		return viewResolver;
+	}
+	@Bean
+	public UrlBasedViewResolver getViewResolver(){
+		UrlBasedViewResolver viewResolver = new UrlBasedViewResolver();
+		viewResolver.setPrefix("/WEB-INF/views/");
+		viewResolver.setSuffix(".jsp");
+		viewResolver.setViewClass(JstlView.class);
+		return viewResolver;
+		
+	}
+	
+	@Bean
+	public MessageSource messageSource() {
+	    ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+	    messageSource.setBasename("messages");
+	    return messageSource;
+	}
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry){
+		registry.addResourceHandler("*/assets/**").addResourceLocations("/assets");
+		
+	}
+}
+
---
+++ src/main/java/com/mobile/store/controller/EmployeeController.java
@@ -0,0 +1,20 @@
+package com.mobile.store.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mobile.store.service.StoreService;
+
+
+@RestController
+public class EmployeeController {
+	
+	private StoreService storeService;
+	
+	
+	
+	public String addEmployee(){
+		return null;
+		
+	}
+
+}
